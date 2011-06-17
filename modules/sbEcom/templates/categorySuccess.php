c576e00c52d34c5efb0f4b4f0754c0e10cae7c34
Giles Smith <giles@superrb.com>
2011-06-17 17:48:48 +0100

    Basic category view

--- modules/sbEcom/templates/categorySuccess.php
+++ modules/sbEcom/templates/categorySuccess.php
@@ -1,8 +1,8 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<h3>This is the category detail page</h3>
+<?php a_slot('ecom-category-detail', 'aRichText'); ?>
+<?php if(count($products) > 0): ?>
+<ul class="ecom-category-product-list">
+	<?php foreach($products as $product): ?>
+	<li><?php include_partial('sbEcom/productExcerpt', array('product' => $product)); ?></li>
+	<?php endforeach; ?>
+</ul>
+<?php endif; ?>
