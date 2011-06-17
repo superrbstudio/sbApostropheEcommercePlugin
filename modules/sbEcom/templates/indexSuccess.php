85151e1d05a5b667d51963b87611bc1a8fec1c9b
Giles Smith <giles@superrb.com>
2011-06-17 17:46:39 +0100

    Basic view output

--- modules/sbEcom/templates/indexSuccess.php
+++ modules/sbEcom/templates/indexSuccess.php
@@ -1,8 +1,18 @@
-<?php
+<?php echo a_slot('ecom-featured-products', 'aRichText'); ?>
+<?php if(count($featuredProducts) > 0): ?>
+<ul class="ecom-featured-products">
+	<?php foreach($featuredProducts as $product): ?>
+	<li><?php include_partial('sbEcom/productExcerpt', array('product' => $product)); ?></li>
+	<?php	endforeach; ?>
+</ul>
+<?php endif; ?>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<h3>This is the shop index template</h3>
+
+<?php echo a_slot('ecom-product-categories', 'aRichText'); ?>
+<?php if(count($categories) > 0): ?>
+<ul class="ecom-product-categories"">
+	<?php foreach($categories as $category): ?>
+	<li><?php include_partial('sbEcom/categoryExcerpt', array('category' => $category)); ?></li>
+	<?php	endforeach; ?>
+</ul>
+<?php endif; ?>
