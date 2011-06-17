4e0cf187b82ad545aff7d871692a3b803cd181c5
Giles Smith <giles@superrb.com>
2011-06-17 17:43:34 +0100

    created base ecom components class

---
+++ lib/actions/BasesbEcomComponents.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Base sbEcom components for the sbApostropheEcommerce Plugin
+ * 
+ * @package    sbApostropheEcommercePlugin
+ * @subpackage sbEcom
+ * @author     Giles Smith
+ */
+abstract class BasesbEcomComponents extends aComponents 
+{
+}
