40adca5f4843ad1510e702f5ef357686ccede584
Giles Smith <giles@superrb.com>
2011-06-17 17:46:57 +0100

    Moved all actions to a base class to allow extension

--- modules/sbEcom/actions/actions.class.php
+++ modules/sbEcom/actions/actions.class.php
@@ -1,24 +1,12 @@
 <?php
 
-class sbEcomActions extends aEngineActions 
+/**
+ * sbEcom actions.
+ * 
+ * @package    sbApostropheEcommercePlugin
+ * @subpackage sbEcom
+ * @author     Giles Smith
+ */
+class sbEcomActions extends BasesbEcomActions 
 {
-	public function executeIndex(sfWebRequest $request)
-	{
-		// get all categories that have products
-		
-	}
-	
-	public function executeCategory(sfWebRequest $request)
-	{
-		// verify category exists
-		
-		// get all products in category and apply tag filters
-		
-	}
-	
-	public function executeProduct(sfWebRequest $request)
-	{
-		// verify product and load
-		
-	}
 }
