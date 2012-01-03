56b75c5c2a08e4e3c40f6de44e61a71d5845db86
Giles Smith <giles@superrb.com>
2012-01-03 14:08:01 +0000

    Extendible components

---
+++ modules/sbEcomAdmin/actions/components.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * sbEcomAdmin Components.
+ *
+ * @package    sbEcommercePlugin
+ * @subpackage sbEcomAdmin
+ * @author     Giles Smith <tech@superrb.com>
+ */
+class sbEcomAdminComponents extends BasesbEcomAdminComponents
+{
+	//put your code here
+}
+
+?>
