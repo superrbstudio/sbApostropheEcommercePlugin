b7de62333cd390890730c4601ec7d045c9151721
Giles Smith <giles@superrb.com>
2012-01-03 11:38:53 +0000

    Admin area for product brands

--- config/sbApostropheEcommercePluginConfiguration.class.php
+++ config/sbApostropheEcommercePluginConfiguration.class.php
@@ -35,7 +35,8 @@ class sbApostropheEcommercePluginConfiguration extends sfPluginConfiguration
     if ($user->hasCredential('sb_ecom_product_admin'))
     {
       aTools::addGlobalButtons(array(
-        new aGlobalButton('products', 'Products', '@sb_ecom_product_sbEcomAdmin', 'a-blog-btn')
+        new aGlobalButton('sb-ecom-products', 'Products', '@sb_ecom_product_sbEcomAdmin', 'a-blog-btn'),
+				new aGlobalButton('sb-ecom-brands', 'Brands', '@sb_ecom_product_sbEcomBrandAdmin', 'a-blog-btn')
       ));
     }
   }
---
+++ modules/sbEcomBrandAdmin/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/sbEcomBrandAdminGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/sbEcomBrandAdminGeneratorHelper.class.php';
+
+/**
+ * sbEcomAdmin actions.
+ *
+ * @package    asandbox
+ * @subpackage sbEcomAdmin
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sbEcomBrandAdminActions extends autoSbEcomBrandAdminActions
+{
+}
