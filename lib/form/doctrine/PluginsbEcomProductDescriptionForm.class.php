7f35c76dc6bb3aca9747d6a25bd5aff1b148b83c
Giles Smith <giles@superrb.com>
2012-01-03 10:06:58 +0000

    Added Brand model

---
+++ lib/form/doctrine/PluginsbEcomProductDescriptionForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginsbEcomProductDescription form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PluginsbEcomProductDescriptionForm extends BasesbEcomProductDescriptionForm
+{
+}
--- lib/model/doctrine/PluginsbEcomProduct.class.php
+++ lib/model/doctrine/PluginsbEcomProduct.class.php
@@ -12,5 +12,48 @@
  */
 abstract class PluginsbEcomProduct extends BasesbEcomProduct
 {
+	public function getSearchText()
+	{
+		/*$slotContent = '';
 
+		$page = aPageTable::retrieveBySlugWithSlots(aPageTable::getFirstEnginePage('sbJobBoardJob')->slug . "/" . $this->getSlug());
+
+		if($page)
+		{
+			foreach($page->getArea('jobDescription') as $slot)
+			{
+				$slotContent .= ' ' . $slot->getText();
+			}
+		}*/
+
+		return $this->getTitle() . " " . $this->getDescription() . " " . implode(' ', $this->getTags());
+	}
+
+	public function getSummary()
+	{
+		return $this->getTitle() . " " . $this->getDescription();
+	}
+
+	public function postSave($event)
+	{
+		parent::postSave($event);
+
+		aTools::$searchService->update(
+			array(
+				'item' => $this,
+				'text' => $this->getSearchText(),
+				'info' => array('summary' => $this->getSummary()),
+				'culture' => aTools::getUserCulture()));
+	}
+
+	public function postDelete($event)
+	{
+		parent::postDelete($event);
+
+		aTools::$searchService->delete(
+			array(
+				'item' => $this
+			)
+		);
+	}
 }
