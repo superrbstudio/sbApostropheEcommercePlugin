152e320eac602557c22c5558389bcadeac1f048f
Giles Smith <giles@superrb.com>
2012-01-06 16:06:23 +0000

    Removed quick search

--- modules/sbEcom/templates/_quickSearch.php
+++
@@ -1,5 +0,0 @@
-<form action="<?php echo url_for('@sb_ecom_search'); ?>" method="get">
-	<?php echo $form->renderHiddenFields(); ?>
-	<?php echo $form['s']->render(); ?>
-	<input type="submit" value="Search" />
-</form>
