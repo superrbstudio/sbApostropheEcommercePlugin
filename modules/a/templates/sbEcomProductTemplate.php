33fa585f8064995ab971a6354db54bf7cc66b9bb
Giles Smith <giles@superrb.com>
2012-01-18 17:26:34 +0000

    Fixed templates wrong way round

--- modules/a/templates/sbEcomProductTemplate.php
+++ modules/a/templates/sbEcomProductTemplate.php
@@ -1,1 +1,17 @@
-<?php require_once(sfConfig::get('sf_root_dir') . '/plugins/sbApostropheEcommercePlugin/modules/a/templates/sbEcomProductTemplate.php'); ?>
+<?php use_helper('a') ?>
+
+<?php // Defining the <body> class ?>
+<?php slot('a-body-class','sb-ecom-product') ?>
+
+<?php include_component('a', 'standardArea', array('name' => 'body', 'width' => 480, 'toolbar' => 'Main', 'slots' => sbEcomToolkit::productAreaSlots())) ?>
+
+<?php include_component('a', 'standardArea', array('name' => 'sidebar', 'width' => 200, 'toolbar' => 'Sidebar')) ?>
+
+<?php slot('a-footer') ?>
+<div class='a-footer-wrapper clearfix'>
+	<div class='a-footer clearfix'>
+	  <?php include_partial('a/footer') ?>
+		<?php include_partial('aFeedback/feedbackForm'); ?>	
+	</div>
+</div>
+<?php end_slot() ?>
