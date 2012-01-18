b39b6a8bb688c01e3ff2dc06cdfe3b5b5e3f8859
Giles Smith <giles@superrb.com>
2012-01-18 10:22:30 +0000

    Allow for overrides at the project level

--- modules/sbEcomAddToBasketSlot/actions/components.class.php
+++ modules/sbEcomAddToBasketSlot/actions/components.class.php
@@ -1,21 +1,4 @@
 <?php
-class sbEcomAddToBasketSlotComponents extends aSlotComponents
+class sbEcomAddToBasketSlotComponents extends PluginsbEcomAddToBasketSlotComponents
 {
-  public function executeEditView()
-  {
-    // Must be at the start of both view components
-    $this->setup();
-    
-    // Careful, don't clobber a form object provided to us with validation errors
-    // from an earlier pass
-    if (!isset($this->form))
-    {
-      $this->form = new sbEcomAddToBasketSlotEditForm($this->id, $this->slot->getArrayValue());
-    }
-  }
-  public function executeNormalView()
-  {
-    $this->setup();
-    $this->values = $this->slot->getArrayValue();
-  }
 }
